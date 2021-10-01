6014e92249236a6d5fb7e108afe8211c37b208c6
MaxAnte <qzzik97@gmail.com>
2021-10-01 10:28:34 +0300

    message handlers

---
+++ server/handlers/messageHandlers.js
@@ -0,0 +1,52 @@
+const { nanoid } = require("nanoid");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db/messages.json");
+const db = low(adapter);
+
+db.defaults({
+  messages: [
+    {
+      messageId: "1",
+      userId: "1",
+      senderName: "Max",
+      messageText: "Wassup, man?",
+      createdAt: "2021-10-01",
+    },
+    {
+      messageId: "2",
+      userId: "2",
+      senderName: "Lebron",
+      messageText: "Good, fam! You?",
+      createdAt: "2021-10-02",
+    },
+  ],
+}).write();
+
+module.exports = (io, socket) => {
+  const getMessages = () => {
+    const messages = db.get("messages").value();
+    io.in(socket.roomId).emit("messages", messages);
+  };
+
+  const addMessage = (message) => {
+    db.get("messages")
+      .push({
+        messageId: nanoid(8),
+        createdAt: newDate(),
+        ...message,
+      })
+      .write();
+
+    getMessages();
+  };
+
+  const removeMessage = (messageId) => {
+    db.get("messages").remove({ messageId }).write();
+    getMessages();
+  };
+
+  socket.on("message:get", getMessages);
+  socket.on("message:add", addMessage);
+  socket.on("message:remove", removeMessage);
+};
