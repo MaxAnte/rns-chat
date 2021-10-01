d997ebb52d41512459b48e5096bbae1e2ac9a8f9
MaxAnte <qzzik97@gmail.com>
2021-10-01 14:11:37 +0300

    finalization

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,23 @@
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Home } from "./components/Home/home";
+// import { ChatRoom } from './components/ChatRoom'
 
-export default function App() {
-  return <div className="App"></div>;
-}
+const routes = [
+  { path: "/", name: "Home", Component: Home },
+  // { path: "/:roomId", name: "ChatRoom", Component: ChatRoom },
+];
+export const App = () => (
+  <Router>
+    <Container style={{ maxWidth: "512px" }}>
+      <h1 className="mt-2 text-center">React Chat App</h1>
+      <Switch>
+        {routes.map(({ path, Component }) => (
+          <Route key={path} path={path} exact>
+            <Component />
+          </Route>
+        ))}
+      </Switch>
+    </Container>
+  </Router>
+);
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
+import { createGlobalStyle } from "styled-components";
 
-import App from "./app";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "emoji-mart/css/emoji-mart.css";
 
-import "./index.css";
+import { App } from "./app";
+
+const GlobalStyles = createGlobalStyle`
+.card-header {
+  padding: 0.25em 0.5em;
+}
+.card-body {
+  padding: 0.25em 0.5em;
+}
+.card-text {
+  margin: 0;
+}
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
