fe5663eb8acb8967251dd13c2991454c0c401f78
MaxAnte <qzzik97@gmail.com>
2021-10-01 10:35:32 +0300

    user handlers

---
+++ server/handlers/userHandlers.js
@@ -0,0 +1,28 @@
+const users = {
+  1: { username: "Max", online: false },
+  2: { username: "Lebron", online: false },
+};
+
+module.exports = (io, socket) => {
+  const getUsers = () => {
+    io.in(socket.roomId).emit("users", users);
+  };
+
+  const addUser = ({ username, userId }) => {
+    if (!users[userId]) {
+      users[userId] = { username, online: true };
+    } else {
+      users[userId].online = true;
+    }
+    getUsers();
+  };
+
+  const removeUser = (userId) => {
+    users[userId].online = false;
+    getUsers();
+  };
+
+  socket.on("user:get", getUsers);
+  socket.on("user:add", addUsers);
+  socket.on("user:leave", removeUsers);
+};
